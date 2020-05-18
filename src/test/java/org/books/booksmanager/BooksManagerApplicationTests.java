062957fdb4a0ce4f1cf037f6af8d1c18b5626687
Thiago <lemes_vilarinho@yahoo.com.br>
2020-05-18 14:56:34 -0300

    Firt commit

---
+++ src/test/java/org/books/booksmanager/BooksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.books.booksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
